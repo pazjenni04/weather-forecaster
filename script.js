b5c18d469a83c254e849fe5eec966c71c94961b1
Jenni Davila <pazjenni1331@gmail.com>
2021-06-14 22:28:00 -0500

    Added local storage to display on page, added css styles, and api now displaying in correct div

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var currentTime = document.getElementById("time");
 var requestURL = "https://api.openweathermap.org/data/2.5/weather?"
 var searchResults = document.getElementById('search-results');
 var userInput = document.getElementById("input");
-
+var zipresults = document.getElementById("zipcodeResults");
 
 
 //time is displayed at top of page
@@ -15,30 +15,43 @@ function time() {
 time();
 
 
-//function gets weather's API and console log's whether the fetch was successful and console logs the results of the main properties pulling from the data
-function getApi() {
-
-    fetch(requestURL)
-    .then (function (response) {
-        return response.json();
-    
-    })
-    .then(data => displayResults(data.results));
-        
-};
 
 displayResults = data => {
     var name = document.createElement('div');
-    var main = document.createElement('div');
+    var temp = document.createElement('div');
+    var coordinates = document.createElement('div');
     var description = document.createElement('div');
+    var humidty = document.createElement('div');
+    var feelsLike = document.createElement('div');
+    var pressure = document.createElement('div');
+    
 
-    name.innerText = "City name: $(data.name)";
-    main.innerText = "$(data.main)";
-    description.innerText = "Description: $(data.weather[0].description";
+    name.className = "name";
+    temp.className = "temperature";
+    coordinates.className = "coordinates";
+    description.className = "description";
+    humidty.className = "humidity";
+    feelsLike.className = "feelsLike";
+    pressure.className = "pressure";
+    
 
+    name.innerText = `City name: ${data.name}`;
+    temp.innerText = `Temperature: ${data.main.temp} F°`;
+    coordinates.innerText = `Latitude: ${data.coord.lat}, Longitude: ${data.coord.lon}`;
+    description.innerText = `Description: ${data.weather[0].description}`;
+    humidty.innerText = `Humidity: ${data.main.humidity}%`;
+    feelsLike.innerText = `Feels like: ${data.main.feels_like} F°`;
+    pressure.innerText = `Pressure: ${data.main.pressure} hPa`;
+    
+    
     searchResults.appendChild(name);
-    searchResults.appendChild(main);
+    searchResults.appendChild(temp);
+    searchResults.appendChild(coordinates);
     searchResults.appendChild(description);
+    searchResults.appendChild(humidty);
+    searchResults.appendChild(feelsLike);
+    searchResults.appendChild(pressure);
+    
 };
 
 //eventlistener takes the value from the user
@@ -51,14 +64,21 @@ userInput.addEventListener("input", function() {
 //need input and when click search it should fetch api with the parameters set
 //need results displayed on the page
 
+//function gets weather's API and console log's whether the fetch was successful and console logs the results of the main properties pulling from the data
 //when the user clicks on the search button, it will concatenate the url along with the user's input and should relocate the window with the results
 function search(event) {
     event.preventDefault();
 
-    var results = requestURL+ "zip=" + userInput.value + ",us&appid=11a183a9809c0b88f3ea75fbbf8c9613"
-    window.location.href = results;
-    console.log(results);
+    var generatedUrl = requestURL+ "zip=" + userInput.value + ",us&units=imperial&icon&appid=11a183a9809c0b88f3ea75fbbf8c9613"
+    
+    fetch(generatedUrl) 
+        .then(response => response.json())
+        .then(data => displayResults(data))
 
+
+    saveToLocal();
+
+  
 };
 
 
@@ -67,3 +87,28 @@ function search(event) {
 document.getElementById("searchBtn").addEventListener("click", search);
 
 //once get search results, need to create local storage to store results in and display on page
+
+var savedItem = JSON.parse(localStorage.getItem("zips")) || [];
+
+function saveToLocal() {
+
+    var cityzip = userInput.value
+
+    savedItem.push(cityzip);
+
+    localStorage.setItem("zips", JSON.stringify(savedItem));
+
+};
+
+function displayzips() {
+    var displayedsearch = document.createElement('div');
+    displayedsearch.className = "displayzips";
+
+
+    var zipCodes = JSON.parse(localStorage.getItem('zips'));
+    displayedsearch.textContent = zipCodes;
+
+    zipresults.appendChild(displayedsearch);
+}
+
+displayzips();
